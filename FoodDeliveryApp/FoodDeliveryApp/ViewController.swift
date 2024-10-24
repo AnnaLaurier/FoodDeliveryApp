12cabe8eb1a290548ee68b543a877aa95ed0265b
Anna Lavrova <anna.laurier99@gmail.com>
2024-10-24 18:34:20 +0300

    Created base project. removed storyboard references

---
+++ FoodDeliveryApp/FoodDeliveryApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodDeliveryApp
+//
+//  Created by Anna Lavrova on 24.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .gray
+    }
+
+
+}
+
